cf1d75e714c48fcb51509d20ab51bfaa0cf957bb
enduranceshey <endurancesunde@gmail.com>
2023-03-18 12:21:30 -0700

    wheew

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Already 0 (success)
+*/
+
+int main(void)
+{
+	char a;
+	
+	char b;
+	
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'A'; b <= 'Z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+return (0);
+}
+
