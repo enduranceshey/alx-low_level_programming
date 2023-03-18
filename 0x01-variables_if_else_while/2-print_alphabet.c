5c84f256ba16e5b8050f04a5480d1765f318d770
enduranceshey <endurancesunde@gmail.com>
2023-03-18 11:50:18 -0700

    wheew

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*Main - Entry point
+*
+*Return: Already 0 (success)
+*/
+int main(void)
+{
+	char a;
+	
+	for (a='a';a <= 'z';++a)
+	{
+		printf("%c ", a);
+	}
+return (0);}
+
