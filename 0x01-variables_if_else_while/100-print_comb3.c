5eaafdcbd5d5ad0dd3707927df2245415535b1da
enduranceshey <endurancesunde@gmail.com>
2023-03-19 10:12:55 -0700

    wheew

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Already 0 (success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(48 + a);
+		for (b = 1; b < 10; b++)
+		{
+			putchar(48 + b);
+			if (b < 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
