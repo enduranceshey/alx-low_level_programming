519a05d531e9a7ac1f33bb628980ad529a9e6923
enduranceshey <endurancesunde@gmail.com>
2023-03-18 19:00:35 -0700

    wheew

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-/**
-*main - Entry point
-*
-*Return: Already 0 (success)
-*/
 
+/**
+* main - Entry point
+*
+* Return: Already 0 (success)
+*/
 int main(void)
 {
 	int a;
-	
-	for (a = 0; a < 10; a++)
+
+	for (a = '0'; a < '10'; a++)
 	{
 		putchar(48 + a);
 	}
