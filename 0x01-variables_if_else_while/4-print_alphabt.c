91e73e9062f89fd196acb9462c8866cd5722f7f3
enduranceshey <endurancesunde@gmail.com>
2023-03-18 18:39:07 -0700

    wheew

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.sh>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Already 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e')
+		{
+			if (a != 'q')
+			{
+				putchar(a);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
