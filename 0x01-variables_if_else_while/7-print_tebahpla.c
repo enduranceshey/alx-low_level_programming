d09e570f94c9a01288c413823ead36304b088506
enduranceshey <endurancesunde@gmail.com>
2023-03-18 17:17:47 -0700

    wheew

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Already 0 (success)
+*/
+
+int main(void)
+{
+	char a;
+	
+	for (a = 'z'; a > 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
