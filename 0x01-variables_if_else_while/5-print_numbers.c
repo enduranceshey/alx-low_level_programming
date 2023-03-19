7994ce5700ed28bce1cb679d1ad49acb088420fc
enduranceshey <endurancesunde@gmail.com>
2023-03-18 17:48:40 -0700

    wheew

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Already 0 (success)
+*/
+
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
+
