b17771298b62eaf493648101a19c91a5232bdb8e
enduranceshey <endurancesunde@gmail.com>
2023-03-19 10:27:17 -0700

    wheew

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,19 @@
  */
 int main(void)
 {
-	int a, b = 0;
+	int a, b;
 
 	for (a = 0; a < 10; a++)
 	{
 		putchar(48 + a);
-		putchar(48 + b++);
-		if (b < 9)
+		for (b = 1; b < a + 2; b++)
 		{
-			putchar(44);
-			putchar(32);
+			putchar(48 + b);
+			if (b < 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
 	}
 	putchar('\n');
