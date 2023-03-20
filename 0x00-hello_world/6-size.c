58bc2a0aeb415c3f4ac8ccc5366ad9070a634536
enduranceshey <endurancesunde@gmail.com>
2023-03-20 04:00:36 -0700

    wheew

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Already 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %2d byte(s)", sizeof(char));
+	printf("Size of an int: %2d byte(s)", sizeof(int));
+	printf("Size of a long int: %2d byte(s)", sizeof(long int));
+	printf("Size of a long long long int: %2d byte(s)", sizeof(long long int));
+	printf("Size of a float: %2d byte(s)", sizeof(float));
+return (0);
+}
+
