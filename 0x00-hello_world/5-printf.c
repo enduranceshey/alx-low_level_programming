90388a175b732e583e44e17072f1b4ec5b80e2c2
enduranceshey <endurancesunde@gmail.com>
2023-03-20 03:38:54 -0700

    wheew

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main()
+
+/**
+ * main - Entry point
+ *
+ * Return: Already 0 (success)
+ */
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+	printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
+
