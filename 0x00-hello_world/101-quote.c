f36c49b41f35f3bae5df1e8742bd8037db5f6480
enduranceshey <endurancesunde@gmail.com>
2023-03-20 05:00:52 -0700

    wheew

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Already 1 (sucess)
+ */
+int main(void)
+{
+	int i;
+
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	for (i = 0; i <= 57; i++)
+	{
+		putchar(quote[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
