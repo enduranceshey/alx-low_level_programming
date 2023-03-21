3e38c9ff22a0afacac8f81352ad6fa304df964ea
enduranceshey <endurancesunde@gmail.com>
2023-03-20 17:41:19 -0700

    Doing hard things

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,18 +17,15 @@ int main(void)
 			{
 				for (d = 0; d < 10; d++)
 				{
-					if (a <= 9 && b < 8)
+					putchar(48 + a);
+					putchar(48 + b);
+					putchar(32);
+					putchar(48 + c);
+					putchar(48 + d);
+					if (a + b + c + d < 35)
 					{
-						putchar(48 + a);
-						putchar(48 + b);
+						putchar(44);
 						putchar(32);
-						putchar(48 + c);
-						putchar(48 + d);
-						if (a + b + c + d < 35)
-						{
-							putchar(44);
-							putchar(32);
-						}
 					}
 				}
 			}
