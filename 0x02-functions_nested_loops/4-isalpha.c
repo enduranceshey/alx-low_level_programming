0755bec9c215a8cf541fafa0c5414b78bd290eff
enduranceshey <endurancesunde@gmail.com>
2023-03-22 10:09:38 -0700

    wheew

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * _isalpha - chacks for alphabet characters
+ * @c: character to be checked
+ *
+ * Return: 1 if success else 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
