ae44a35c1613c2d3919fad7b4355d43a7691d996
enduranceshey <endurancesunde@gmail.com>
2023-03-21 22:59:54 -0700

    doing hard things

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 
-int _putchar(char txt[]);
-
 /**
  *  main - Entry point
  *
@@ -10,17 +8,6 @@ int _putchar(char txt[]);
 int main(void)
 {
 	_putchar("_putchar\n");
-	return (0);
-}
-/**
- * _putchar - Printing function
- * @txt: this is the text to be printed
- *
- * Return: Already 0 (success)
- */
-int _putchar(char txt[])
-{
-	printf("%s", txt);
 	return (0);
 }
 
