ea99535d08e15ba5264fdd57c55a2207c54193b9
enduranceshey <endurancesunde@gmail.com>
2023-03-22 15:16:59 -0700

    wheew

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	int sum = 0, i, a, b;
+	int sum = 0, i;
 
 	for (i = 0; i < 1024; i++)
 	{
 		a = i % 3;
 		b = i % 5;
-		if (a = 0 || b = 0)
+		if (a == 0 || b == 0)
 		{
 			sum = sum + i;
 		}
