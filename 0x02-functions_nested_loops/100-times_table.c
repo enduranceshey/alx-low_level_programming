be94dc4deb117cad49d208efa95eee9f8ea68dbe
enduranceshey <endurancesunde@gmail.com>
2023-03-22 14:51:26 -0700

    wheew

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,37 +8,46 @@
  */
 void print_times_table(int n)
 {
-	int a, b, c, d, e;
+	int a, b, c;
 
-	if (n > 15 || n < 0)
+	if (n >= 0 && n <= 15)
 	{
-	}
-	for (a = 0; a <= n; a++)
-	{
-		for (b = 0; b <= n; b++)
+		for (a = 0; a <= n; a++)
 		{
-			c = a * b;
-			if (c > n)
+			for (b = 0; b <= n; b++)
 			{
-				d = c % 10;
-				e = (c - d) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(e + '0');
-			}
-			else
-			{
-				if (b != 0)
+				c = a * b;
+				
+				if (c > 99)
 				{
-					_putchar(44);
+					_putchar(',');
 					_putchar(32);
-					_putchar(32);
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
 				}
-				_putchar(c + '0');
+				else if (c > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else
+				{
+					if (b != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(c + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
 
