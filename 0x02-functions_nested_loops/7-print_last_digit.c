0c58f184d279d0b021dcb2f216947760dd3c306f
enduranceshey <endurancesunde@gmail.com>
2023-03-22 10:56:29 -0700

    wheew

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @c: input number
+ *
+ * Return: 0 success
+ */
+int print_last_digit(int c)
+{
+	int d;
+
+	d = c % 10;
+	_putchar(d);
+	return (0);
+}
+
