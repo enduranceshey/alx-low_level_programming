8f24ab06c99eaa15a21c4455eb0fc175d53b2797
enduranceshey <endurancesunde@gmail.com>
2023-03-22 12:22:13 -0700

    wheew

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * add - sums up two integers
+ * @a: integers to be summed up
+ * @b: second number
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum a + b;
+	return (sum);
+}
+
