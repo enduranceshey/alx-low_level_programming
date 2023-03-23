00a7996eeb27afb9e6bf90f510c4cc3721f83bef
enduranceshey <endurancesunde@gmail.com>
2023-03-23 11:31:55 -0700

    It's a beautiful day~

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: the first integer
+ * @b: thesecond integer
+ *
+ * Return: multiple of a & b (success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
