1b3d6dccd744900d23d8bd0d5909fef896d11306
enduranceshey <endurancesunde@gmail.com>
2023-03-23 13:10:31 -0700

    wheew

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_line - draw a line in the terminal
+ * @n: length of line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(45);
+	}
+}
+
