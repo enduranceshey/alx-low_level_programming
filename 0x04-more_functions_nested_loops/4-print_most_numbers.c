8f21311749bc5b62af59b791ff57dc4465cbe900
enduranceshey <endurancesunde@gmail.com>
2023-03-23 11:48:53 -0700

    It's a beautiful day~

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_numbers - prints digits from 1 to 9 except 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(48 + i);
+		}
+	}
+	_putchar('\n');
+}
+
