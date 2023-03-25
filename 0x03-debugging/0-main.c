89936f4aa18af988ce189d63bfcec821d6f84f74
enduranceshey <endurancesunde@gmail.com>
2023-03-24 18:11:23 -0700

    up all night

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * main - checks if an intteger is +ve or -ve
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int a;
+
+	a  = 0;
+	positive_or_negetive(a);
+	return (0);
+}
+
