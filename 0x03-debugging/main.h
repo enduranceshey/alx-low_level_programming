27e865afa28a64571b0df77114888f14d46ef704
enduranceshey <endurancesunde@gmail.com>
2023-03-24 19:37:45 -0700

    up all night

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,7 @@
 
 void positive_or_negetive(int i);
 int largest_number(int a, int b, int c);
+int convert_day(int mont, int day);
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
